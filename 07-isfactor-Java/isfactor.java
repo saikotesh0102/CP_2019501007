129a33cef8cc7c54236a0ba6603eaccb3d7ec0a6
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-13 19:27:06 +0530

    All Test Cases Passed

--- 07-isfactor-Java/isfactor.java
+++ 07-isfactor-Java/isfactor.java
@@ -3,9 +3,19 @@
 // # if f is a factor of n, and False otherwise. 
 // # Note that every integer is a factor of 0.
 
+import java.lang.*;
+
 class isfactor {
 	public boolean fun_isfactor(int f, int n){
 		// your code goes here
-		return false;
+		if(f == 0 && n > 0){
+			return false;
+		}else if(n == 0 && f == 0){
+			return true;
+		}else if(n % Math.abs(f) == 0){
+			return true;
+		}else{
+			return false;
+		}
 	}
 }
