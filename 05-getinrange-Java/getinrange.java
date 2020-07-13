56a92eb272d34d0ad8f2c76633c2146b2c5df592
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-13 18:57:49 +0530

    All Test Cases Passed

--- 05-getinrange-Java/getinrange.java
+++ 05-getinrange-Java/getinrange.java
@@ -1,3 +1,5 @@
+import javax.print.attribute.standard.ReferenceUriSchemesSupported;
+
 // # Write the function getInRange(x, bound1, bound2) which takes 3 int values
 // # x, bound1, and bound2, where bound1 is not necessarily less than bound2. 
 // # If x is between the two bounds, just return it unmodified. Otherwise, 
@@ -7,6 +9,22 @@
 class getinrange {
 	public int fun_getinrange(int x, int bound1, int bound2){
 		// Your code goes here
-		return -1;
+		if(bound1 < bound2){
+			if (x < bound1){
+				return bound1;
+			}else if (x > bound2){
+				return bound2;
+			}else{
+				return x;
+			}
+		}else{
+			if (x > bound1){
+				return bound1;
+			}else if (x < bound2){
+				return bound2;
+			}else{
+				return x;
+			}
+		}
 	}
 }
