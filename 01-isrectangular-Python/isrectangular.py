4e80fbe2b4986a6fd65f9e8e469a5c0ac86f31e6
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-13 17:19:48 +0530

    Test Cases Passed

--- 01-isrectangular-Python/isrectangular.py
+++ 01-isrectangular-Python/isrectangular.py
@@ -2,7 +2,6 @@
 # list L and returns True  if it is rectangular, so each row has
 #  the same number of elements. Return False otherwise.
 
-
 def fun_isrectangular(l):
 	t = len(l[0])
 	for i in l:
