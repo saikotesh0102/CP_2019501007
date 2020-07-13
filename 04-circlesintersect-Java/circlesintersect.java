0aac89f7f34424357d45c4127afe34a33dc141ee
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-13 18:52:14 +0530

    All TestCases Passed

--- 04-circlesintersect-Java/circlesintersect.java
+++ 04-circlesintersect-Java/circlesintersect.java
@@ -6,6 +6,15 @@
 
 class circlesintersect {
 	public boolean fun_circlesintersect(int x1, int y1, int r1, int x2, int y2, int r2){
-		return false;
+		int dist = ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
+		int radi = (r1 + r2) * (r1 + r2);
+
+		if(dist == radi){
+			return true;
+		}else if(dist < radi){
+			return true;
+		}else{
+			return false;
+		}
 	}
 }
