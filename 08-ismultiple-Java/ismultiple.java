ed31a4ad2bd085c42813ec57d4c8e0979787b203
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-13 19:48:10 +0530

    All Test cases passed

--- 08-ismultiple-Java/ismultiple.java
+++ 08-ismultiple-Java/ismultiple.java
@@ -3,10 +3,19 @@
 // # Note that 0 is a multiple of every integer including itself. 
 // # Also, you should make constructive use of the isFactor function you just wrote above.
 
+import java.lang.*;
 
 class ismultiple {
 	public boolean fun_ismultiple(int m, int n){
 		// your code goes here
-		return false;
+		if(n == 0 && m > 0){
+			return false;
+		}else if(m == 0 && n == 0){
+			return true;
+		}else if(m % Math.abs(n) == 0){
+			return true;
+		}else{
+			return false;
+		}
 	}
 }
