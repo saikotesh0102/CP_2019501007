94832c407618ea4714c26483337a09d320bc4592
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-14 11:47:27 +0530

    Commit

--- 01-pythonbasics-Python/pythonbasics.py
+++ 01-pythonbasics-Python/pythonbasics.py
@@ -4,8 +4,7 @@
 # Return the string with "return".
 # You can only have the string once in your code.
 # Don't just copy/paste it 5 times into a single variable!
-#Sai Koteswara Rao Ch
+# Sai Koteswara Rao Chinni
 
 def show_excitement():
-    # Your code goes here!
     return "I am super excited for this course! " * 5
