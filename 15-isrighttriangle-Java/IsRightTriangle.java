f9ee6322adcfc1b2565e72bd7a8af70ae83bdb79
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-14 09:32:59 +0530

    All Test Cases Passed

--- 15-isrighttriangle-Java/IsRightTriangle.java
+++ 15-isrighttriangle-Java/IsRightTriangle.java
@@ -5,9 +5,27 @@
 // distance(x1, y1, x2, y2), which you might call several times. Also, remember to use
 // almostEqual (instead of ==) when comparing floats.
 
+import java.lang.*;
+
 public class IsRightTriangle {
 	public boolean isRightTriangle(int x1, int y1, int x2, int y2, int x3, int y3) {
 		// Your code goes here
-		return false;
+		int a = distance(x1, y1, x2, y2);
+		int b = distance(x2, y2, x3, y3);
+		int c = distance(x1, y1, x3, y3);
+		
+		if((Math.pow(a,2) + Math.pow(b,2)) == Math.pow(c,2)){
+			return true;
+		}else if((Math.pow(b,2) + Math.pow(c,2)) == Math.pow(a,2)){
+			return true;
+		}else if((Math.pow(a,2) + Math.pow(c,2)) == Math.pow(b,2)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	public int distance(int x1, int y1, int x2, int y2){
+		return (int)Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
 	}
 }
