5104f668451a43d9df87c21baecbb94b3320e3bb
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-14 14:03:07 +0530

    Jul 14  14:03:07

--- 03-recursion-Java/recursion.java
+++ 03-recursion-Java/recursion.java
@@ -1,5 +1,14 @@
 class recursion {
 	public int get_fib(int value){
-	    return -1; 
+		if(value == 0){
+			return 0;
+		}else if(value == 1){
+			return 1;
+		}
+	    return get_fib(value - 1) + get_fib(value - 2);
+	}
+	
+	public static void main(String[] args) {
+		
 	}
 }
