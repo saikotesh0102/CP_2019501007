51c28bf1c2034e7e5eafd931d3de0e8247f6bc1f
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-16 09:37:11 +0530

    Jul 16  09:37:10

--- 02-nth_happy_prime-Java/nth_happy_prime.java
+++ 02-nth_happy_prime-Java/nth_happy_prime.java
@@ -6,6 +6,62 @@
 class nth_happy_prime {
 	public int fun_nth_happy_prime(int n){
 		// your code goes here
-		return 0;	
+		if(n == 0){
+			return 7;
+		}	
+		int start = 2;
+		int count = 0;
+		while(true){
+			if(isHappyNumber(start) && isPrime(start)){
+				count = count + 1;
+				if(count == n){
+					return start;
+				}
+			}
+			start = start + 1;
+		}
+	}
+
+	public boolean isPrime(int n) { 
+        // Corner case 
+        if (n <= 1) 
+            return false; 
+  
+        // Check from 2 to n-1 
+        for (int i = 2; i < n; i++) 
+            if (n % i == 0) 
+                return false; 
+        return true; 
+    }
+
+	public boolean isHappyNumber(int n) {
+		// your code goes here
+		int num = Math.abs(n);
+		if(num == 1 || n == 7){
+			return true;
+		}
+		int sum = num; 
+		int x = num;
+
+		while(sum > 9){
+			sum = 0;
+			while(x > 0){
+				int d = x % 10;
+				sum = sum + d * d;
+				x = x / 10;
+			}
+			if(sum == 1){
+				return true;
+			}
+			x = sum;
+		}
+		if(sum == 7){
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		
 	}
 }
