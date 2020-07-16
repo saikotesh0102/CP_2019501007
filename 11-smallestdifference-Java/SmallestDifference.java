2486bff1b5f681659296171e22a66c8e5c09d234
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-16 12:53:52 +0530

    Jul 16  12:53:52

--- 11-smallestdifference-Java/SmallestDifference.java
+++ 11-smallestdifference-Java/SmallestDifference.java
@@ -3,12 +3,18 @@
 // integers in the list. If the list is empty, return -1. For example:
 //       assert(smallestDifference([19,2,83,6,27]) == 4)
 // The two closest numbers in that list are 2 and 6, and their difference is 4.
+
 import java.util.*;
 
 public class SmallestDifference {
 
     public int smallestDifference(int[] a) {
         // Your code goes here
-        return -1;
+        Arrays.sort(a);
+        return a[1] - a[0];
+    }
+
+    public static void main(String[] args) {
+        
     }
 }
