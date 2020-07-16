680f17883eec5ba5423849b78e8ecb93b3d1157f
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-16 11:19:33 +0530

    Jul 16  11:19:33

--- 06-replace-Java/replace.java
+++ 06-replace-Java/replace.java
@@ -6,7 +6,15 @@
 
 class replace {
 	public String fun_replace(String s1, String s2, String s3){
-		return s1.replace(s2, s3);
+		// return s1.replace(s2, s3);
+		int i = s1.indexOf(s2);
+		if(i < 0){
+			return s1;
+		}
+		String before = s1.substring(0, i);
+		String after = s1.substring(i + s2.length());
+
+		return before + s3 + after;
 	}
 	
 	public static void main(String[] args) {
