bb1958b1860d2d3a2bc5cc00946aaa470c415270
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-17 11:37:13 +0530

    Jul 17  11:37:12

--- 05-binarytreepractice-Java/BinaryTree.java
+++ 05-binarytreepractice-Java/BinaryTree.java
@@ -1,4 +1,3 @@
-
 class Node {
 	public int value;
 	public Node left, right;
@@ -13,17 +12,30 @@ public class BinaryTree {
 	public Node root;
 	
 	public BinaryTree(int value) {
-        // Your code goes here
+		// Your code goes here
+		this.root = new Node(value);
 	}
 
 	public boolean search(int value) {
 		// Your code goes here
-		return true;
+		return search_Node(root, value);
 	}
 
 	private boolean search_Node(Node node, int value) {
 		// Your code goes here
+		while(node != null){
+			if(value < node.value){
+				node = node.left;
+			}else if(value > node.value){
+				node = node.right;
+			}else{
+				return true;
+			}
+		}
 		return false;
+	}
 
+	public static void main(String[] args) {
+		
 	}
 }
