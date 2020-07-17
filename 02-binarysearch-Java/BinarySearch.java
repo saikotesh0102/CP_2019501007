76528b469dd12c4dc28f703ea1c3bfc47b8989fc
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-17 10:01:56 +0530

    All Test Cases Passed

--- 02-binarysearch-Java/BinarySearch.java
+++ 02-binarysearch-Java/BinarySearch.java
@@ -1,6 +1,23 @@
 class BinarySearch {
 	public int binary_search(int[] arr, int value){
-        // Your code goes here
+		// Your code goes here
+		int flag = 0;
+		int len = arr.length - 1;
+		while(flag <= len){
+			int m = (flag + len) / 2;
+
+			if(arr[m] < value){
+				flag = m + 1;
+			}else if(arr[m] > value){
+				len = m - 1;
+			}else{
+				return m;
+			}
+		}
         return -1;
+	}
+
+	public static void main(String[] args) {
+		
 	}
 }
