b6f0d3430525cb73e3312405d42e718113e004fb
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-17 10:28:43 +0530

    Jul 17  10:28:43

--- 04-stringkeys-Java/StringKeys.java
+++ 04-stringkeys-Java/StringKeys.java
@@ -13,15 +13,18 @@ public class StringKeys {
 	public int lookup(String key) {
 		// Your code goes here
 		return 1;
-
 	}
 
 	public int calculate_hash_value(String key) {
 		// Your code goes here
-		return 1;
+		return ((int)(key.charAt(0)) * 100) + (int)(key.charAt(1));
 	}
 
 	public void show_bucket(String key) {
 		// Your code goes here
+	}
+
+	public static void main(String[] args) {
+		
 	}
 }
