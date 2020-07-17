4cd1a6bcfb8f1d7d7efcdd651b8fd59582b0f318
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-17 11:37:42 +0530

    Minor change

--- 05-binarytreepractice-Java/TestJUnit.java
+++ 05-binarytreepractice-Java/TestJUnit.java
@@ -16,36 +16,36 @@ public class TestJUnit {
    @Test
    public void testCase1() {
    	BinaryTree s = new BinaryTree(1);
-   	// s.root.left = new Node(2);
-   	// s.root.right = new Node(3);
-   	// s.root.left.left = new Node(4);
-   	// s.root.left.right = new Node(5);
-   	assertEquals(s.search(1), true);
-   	// assertEquals(s.search(6), false);
+   	s.root.left = new Node(2);
+   	s.root.right = new Node(3);
+   	s.root.left.left = new Node(4);
+   	s.root.left.right = new Node(5);
+   	assertEquals(s.search(4), true);
+   	assertEquals(s.search(6), false);
    }
     
-   // @Test
-   // public void testCase2() {
-   // 	BinaryTree s = new BinaryTree(1);
-   // 	assertEquals(s.search(4), false);
-   // 	assertEquals(s.search(1), true);
-   // }
+   @Test
+   public void testCase2() {
+   	BinaryTree s = new BinaryTree(1);
+   	assertEquals(s.search(4), false);
+   	assertEquals(s.search(1), true);
+   }
    
-   // @Test
-   // public void testCase3() {
-   // 	BinaryTree s = new BinaryTree(1);
-   //    s.root.left = new Node(1);  
-   //    s.root.left.left = new Node(3);  
-   //    s.root.left.left.left = new Node(7);  
-   //    s.root.left.right = new Node(4);  
-   //    s.root.left.right.left = new Node(8);  
-   //    s.root.left.right.right = new Node(9);  
-   //    s.root.right = new Node(2);  
-   //    s.root.right.left = new Node(5);  
-   //    s.root.right.right = new Node(6);  
-   // 	assertEquals(s.search(4), true);
-   // 	assertEquals(s.search(6), true);
-   // 	assertEquals(s.search(16), false);
-   // 	assertEquals(s.search(8), true);
-   // }
+   @Test
+   public void testCase3() {
+   	BinaryTree s = new BinaryTree(1);
+      s.root.left = new Node(1);  
+      s.root.left.left = new Node(3);  
+      s.root.left.left.left = new Node(7);  
+      s.root.left.right = new Node(4);  
+      s.root.left.right.left = new Node(8);  
+      s.root.left.right.right = new Node(9);  
+      s.root.right = new Node(2);  
+      s.root.right.left = new Node(5);  
+      s.root.right.right = new Node(6);  
+   	assertEquals(s.search(4), true);
+   	assertEquals(s.search(6), true);
+   	assertEquals(s.search(16), false);
+   	assertEquals(s.search(8), true);
+   }
 }
