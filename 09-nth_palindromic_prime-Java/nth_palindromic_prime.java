c642bc5365b4f232aa0ffd137dc7f6ab69f01cd1
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-17 12:49:22 +0530

    Jul 17  12:49:21

--- 09-nth_palindromic_prime-Java/nth_palindromic_prime.java
+++ 09-nth_palindromic_prime-Java/nth_palindromic_prime.java
@@ -6,6 +6,52 @@
 
 class nth_palindromic_prime {
 	public int fun_nth_palindromic_prime(int n){
-		return 0;
+		if(n == 0){
+			return 2;
+		}
+
+		int flag = 3;
+		int count = 0;
+		while(true){
+			if(isPrime(flag) && isPalindrome(flag)){
+				count = count + 1;
+				if(count == n){
+					return flag;
+				}
+			}
+			flag = flag + 2;
+		}
+	}
+
+	public boolean isPrime(int n) { 
+        // Corner case 
+        if (n <= 1) 
+            return false; 
+  
+        // Check from 2 to n-1 
+        for (int i = 2; i < n; i++) 
+            if (n % i == 0) 
+                return false; 
+        return true; 
+	}
+
+	public boolean isPalindrome(int n){
+		int reverse = 0;
+		int digit = n;
+
+		while(n != 0){
+			reverse = reverse * 10 + (n % 10);
+			n = n / 10;
+		}
+
+		if(digit == reverse){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	public static void main(String[] args) {
+		
 	}
 }
