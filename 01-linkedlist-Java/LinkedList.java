6f8864505edce8c156801968e557faa2b6c693d4
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-17 09:37:08 +0530

    Jul 17  09:37:08

--- 01-linkedlist-Java/LinkedList.java
+++ 01-linkedlist-Java/LinkedList.java
@@ -1,4 +1,3 @@
-
 class Element{
 	int value;
 	Element next;
@@ -16,6 +15,15 @@ public class LinkedList{
 
 	public void append(Element new_element){
 		// Your code goes here
+		Element current = this.head;
+		if(this.head != null){
+			while(current.next != null){
+				current = current.next;
+			}
+			current.next = new_element; 
+		}else{
+			this.head = new_element;
+		}
 	}
 
 	public Element get_position(int position){
@@ -23,20 +31,43 @@ public class LinkedList{
         // Assume the first position is "1".
         // Return null if position is not in the list
 		// Your code goes here
+		Element current = this.head;
+		int count = 0;
+		while (current != null) { 
+            if (count == position){
+				return current;
+			}
+            count++; 
+            current = current.next; 
+        }
 		return null;
 	}
 
 	public void insert(Element new_element, int position){
-	   // """Insert a new node at the given position.
-       // Assume the first position is "1".
-       // Inserting at position 3 means between
-       // the 2nd and 3rd elements."""
+		// """Insert a new node at the given position.
+		// Assume the first position is "1".
+		// Inserting at position 3 means between
+		// the 2nd and 3rd elements."""
 		// Your code goes here
-
+		if(position == 0){
+			new_element.next = this.head;
+			this.head = new_element;
+		}else{
+			Element current = this.head;
+			while(--position > 0){
+				current = current.next;
+			}
+			new_element.next = current.next;
+			current.next = new_element;
+		}
 	}
 
 	public void delete(int value){
 		// Delete the first node with a given value.
 		// Your code goes here
+	}
+
+	public static void main(String[] args) {
+		
 	}
 }
