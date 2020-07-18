248c49f39e24bcbf0a1a359f8007ff8e1370b5a9
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-18 11:44:35 +0530

    All Testcases Passed

--- 11-ismostlymagicsquare-Java/IsMostlyMagicSquare.java
+++ 11-ismostlymagicsquare-Java/IsMostlyMagicSquare.java
@@ -14,8 +14,50 @@
 // Each row and each column add to 3, but one diagonal adds to 2 and the other to 4.
 
 public class IsMostlyMagicSquare {
-	public boolean isMostlyMagicSquare(int[][] a) {
+	public boolean isMostlyMagicSquare(int[][] arr) {
 		// Your code goes here
-		return false;
+		int sum1 = 0;
+		int sum2 = 0;
+
+		for(int i = 0; i < arr.length; i++) {
+			sum1 = sum1 + arr[i][i];
+		}
+
+		for (int i = 0; i < arr.length; i++){ 
+			sum2 = sum2 + arr[i][arr.length - 1 - i];
+		}
+
+		if(sum1 != sum2){
+			return false;
+		}
+
+		for (int i = 0; i < arr.length; i++) { 
+  
+            int rowSum = 0; 
+            for (int j = 0; j < arr.length; j++){
+				rowSum = rowSum + arr[i][j];
+			} 
+   
+            if (rowSum != sum1){
+				return false; 
+			}
+		}
+		
+		for (int i = 0; i < arr.length; i++) { 
+  
+            int colSum = 0; 
+            for (int j = 0; j < arr.length; j++){
+				colSum = colSum + arr[j][i]; 
+			}
+
+            if (sum1 != colSum){
+				return false; 
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		
 	}
 }
