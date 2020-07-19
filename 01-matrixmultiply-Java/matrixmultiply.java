acbca5014b60f34f39fd2c0b281f2d1c543084ea
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-19 19:25:46 +0530

    Working

--- 01-matrixmultiply-Java/matrixmultiply.java
+++ 01-matrixmultiply-Java/matrixmultiply.java
@@ -5,7 +5,18 @@
 
 class matrixmultiply {
 	public int[][] fun_matrixmultiply(int[][] a, int[][] b){
-		int[][] arr = {{}};
+		int[][] arr = new int[a.length][b[0].length];
+		for(int i = 0; i < a.length; i++){
+			for(int j = 0; j < b[0].length; j++){
+				for(int k = 0; k < a[0].length; k++) {
+					arr[i][j] = arr[i][j] + (a[i][k] * b[k][j]);
+				}
+			}
+		}
 		return arr;
+	}
+
+	public static void main(String[] args) {
+		
 	}
 }
