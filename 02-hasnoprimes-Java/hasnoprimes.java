dcd8144d991952258946cded10a21f18e3f81c60
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-19 18:06:35 +0530

    All Test Cases Passed

--- 02-hasnoprimes-Java/hasnoprimes.java
+++ 02-hasnoprimes-Java/hasnoprimes.java
@@ -2,8 +2,32 @@
 // # and returns True if arr does not contain any primes, and False otherwise.
 
 
-class hasnoprimes {
+public class hasnoprimes {
 	public boolean fun_hasnoprimes(int[][] arr){
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++){
+				if(isPrime(arr[i][j])){
+					return false;
+				}
+			}
+		}
 		return true;
+	}
+
+	public boolean isPrime(int n) { 
+        if (n <= 1){
+			return false; 
+		}
+			
+        for (int i = 2; i < n; i++){
+            if (n % i == 0){
+				return false;
+			}
+		}
+        return true; 
+    }
+
+	public static void main(String[] args) {
+		
 	}
 }
