6733f477cedcc56976d528f55ccfacea49ff25ea
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-21 10:06:53 +0530

    Working

--- 04-fixmostlymagicsquare-Java/FixMostlyMagicSquare.java
+++ 04-fixmostlymagicsquare-Java/FixMostlyMagicSquare.java
@@ -14,8 +14,59 @@
 // Each row and each column add to 3, but one diagonal adds to 2 and the other to 4.
 
 public class FixMostlyMagicSquare {
-	public int[][] fixMostlyMagicSquare(int[][] a) {
+	public int[][] fixMostlyMagicSquare(int[][] arr) {
 		// Your code goes here
+		if(isMostlyMagicSquare(arr)){
+			return arr;
+		}
 		return new int[0][0];
+	}
+
+	public boolean isMostlyMagicSquare(int[][] arr) {
+		// Your code goes here
+		int sum1 = 0;
+		int sum2 = 0;
+
+		for(int i = 0; i < arr.length; i++) {
+			sum1 = sum1 + arr[i][i];
+		}
+
+		for (int i = 0; i < arr.length; i++){ 
+			sum2 = sum2 + arr[i][arr.length - 1 - i];
+		}
+
+		if(sum1 != sum2){
+			return false;
+		}
+
+		for (int i = 0; i < arr.length; i++) { 
+  
+            int rowSum = 0; 
+            for (int j = 0; j < arr.length; j++){
+				rowSum = rowSum + arr[i][j];
+			} 
+   
+            if (rowSum != sum1){
+				return false; 
+			}
+		}
+		
+		for (int i = 0; i < arr.length; i++) { 
+  
+            int colSum = 0; 
+            for (int j = 0; j < arr.length; j++){
+				colSum = colSum + arr[j][i]; 
+			}
+
+            if (sum1 != colSum){
+				return false; 
+			}
+		}
+		return true;
+	}
+
+
+	public static void main(String[] args) {
+		
 	}
 }
