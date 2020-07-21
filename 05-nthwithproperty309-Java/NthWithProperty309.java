6b3b04f2e149bde9a31dce5bd4b3f0662030e19f
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-21 18:54:33 +0530

    Jul 21  18:54:32

--- 05-nthwithproperty309-Java/NthWithProperty309.java
+++ 05-nthwithproperty309-Java/NthWithProperty309.java
@@ -7,6 +7,32 @@
 public class NthWithProperty309 {
 	public int nthWithProperty309(int n) {
 		//Your code goes here
-		return -1;
+		if(n == 0){
+			return 309;
+		}
+		int count = 0;
+		int start = 310;
+		while(true){
+			if(isProperty309(start)){
+				count = count + 1;
+				if(count == n){
+					return start;
+				}
+			}
+			start = start + 1;
+		}
+	}
+
+	public boolean isProperty309(int number){
+		String res = Integer.toString((int)Math.pow(number, 5));
+		if(res.contains("0") && res.contains("1") && res.contains("2") && res.contains("3") && res.contains("4") && res.contains("5") && res.contains("6") && res.contains("7") && res.contains("8") && res.contains("9")){
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		NthWithProperty309 ob = new NthWithProperty309();
+		System.out.println(ob.nthWithProperty309(1));
 	}
 }
