ccdc1f03eea191da657234f0e379538600b946f3
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-21 19:00:44 +0530

    Jul 21  19:00:43

--- 05-nthwithproperty309-Java/NthWithProperty309.java
+++ 05-nthwithproperty309-Java/NthWithProperty309.java
@@ -25,8 +25,8 @@ public class NthWithProperty309 {
 
 	public boolean isProperty309(int number){
 		String res = Integer.toString((int)Math.pow(number, 5));
+		System.out.println(number);
 		if(res.contains("0") && res.contains("1") && res.contains("2") && res.contains("3") && res.contains("4") && res.contains("5") && res.contains("6") && res.contains("7") && res.contains("8") && res.contains("9")){
-			System.out.println(number);
 			return true;
 		}
 		return false;
