305486b455328aa0b3cd24f52fa474084ed2a6a9
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-21 11:11:28 +0530

    All Testcases passsed

--- 01-matrixmultiply-Java/matrixmultiply.java
+++ 01-matrixmultiply-Java/matrixmultiply.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 // # Write the function matrixMultiply(m1, m2) that takes two 2d arrays 
 // # (that we will consider to be matrices) and returns a new 2d array that 
 // # is the result of multiplying the two matrices. Return [[]] if the 
@@ -7,24 +5,24 @@ import java.util.Arrays;
 
 class matrixmultiply {
 	public int[][] fun_matrixmultiply(int[][] a, int[][] b){
+		int[][] arr = {{}};
 		if(a[0].length != b.length){
-			return new int[][]{};
-		}
-		int[][] arr = new int[a.length][b[0].length];
-		for(int i = 0; i < a.length; i++){
-			for(int j = 0; j < b[0].length; j++){
-				for(int k = 0; k < a[0].length; k++) {
-					arr[i][j] = arr[i][j] + (a[i][k] * b[k][j]);
+			return arr;
+		}else{
+			int[][] res = new int[a.length][b[0].length];
+			for(int i = 0; i < a.length; i++){
+				for(int j = 0; j < b[0].length; j++){
+					res[i][j] = 0;
+					for(int k = 0; k < b.length; k++) {
+						res[i][j] = res[i][j] + (a[i][k] * b[k][j]);
+					}
 				}
 			}
+			return res;
 		}
-		return arr;
 	}
 
 	public static void main(String[] args) {
-		matrixmultiply ob = new matrixmultiply();
-		int[][] a = {{1,3,5}, {2,4,6},{2,5,7}};
-      	int[][] b = {{1,3,2,2},{2,4,5,1}};
-		System.out.println(Arrays.toString(ob.fun_matrixmultiply(a, b)));
+		
 	}
 }
