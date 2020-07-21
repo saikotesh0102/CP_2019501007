cf787800beba011e102257f4908cc1e252b49816
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-21 10:42:31 +0530

    working

--- 01-matrixmultiply-Java/TestJUnit.java
+++ 01-matrixmultiply-Java/TestJUnit.java
@@ -17,18 +17,18 @@ import java.util.*;
 
 public class TestJUnit {
 
-   // @Test
-   // public void testCase1() {
-   //    matrixmultiply s = new matrixmultiply();
-   //    int[][] arr1 = {{1,3}, {2,4},{2,5}};
-   //    int[][] arr2 = {{1,3,2,2},{2,4,5,1}};
-   //    int[][] res1 = {{7,15,17,5}, {10, 22, 24, 8},{12, 26, 29, 9}};
-   //    int[][] arr3 = {{1},{2,4},{2,5}};
-   //    int[][] arr4 = {{1,3,2,2},{2,4,5,1}};
-   //    int[][] res2 = {{}};
-   //    assertTrue("1.",  Arrays.deepEquals(res1, s.fun_matrixmultiply(arr1, arr2)));
-   //    assertTrue("2.",  Arrays.deepEquals(res2, s.fun_matrixmultiply(arr3, arr4)));
-   // }
+   @Test
+   public void testCase1() {
+      matrixmultiply s = new matrixmultiply();
+      int[][] arr1 = {{1,3}, {2,4},{2,5}};
+      int[][] arr2 = {{1,3,2,2},{2,4,5,1}};
+      int[][] res1 = {{7,15,17,5}, {10, 22, 24, 8},{12, 26, 29, 9}};
+      int[][] arr3 = {{1},{2,4},{2,5}};
+      int[][] arr4 = {{1,3,2,2},{2,4,5,1}};
+      int[][] res2 = {{}};
+      assertTrue("1.",  Arrays.deepEquals(res1, s.fun_matrixmultiply(arr1, arr2)));
+      assertTrue("2.",  Arrays.deepEquals(res2, s.fun_matrixmultiply(arr3, arr4)));
+   }
 
    @Test
    public void testCase2() {
@@ -36,11 +36,11 @@ public class TestJUnit {
       int[][] arr1 = {{1,3,5}, {2,4,6},{2,5,7}};
       int[][] arr2 = {{1,3,2,2},{2,4,5,1}};
       int[][] res1 = {{}};
-      // int[][] arr3 = {{1,3},{2,4}};
-      // int[][] arr4 = {{1,3,2,2},{2,4,5,1}};
-      // int[][] res2 = {{7,15,17,5}, {10,22,24,8}};
+      int[][] arr3 = {{1,3},{2,4}};
+      int[][] arr4 = {{1,3,2,2},{2,4,5,1}};
+      int[][] res2 = {{7,15,17,5}, {10,22,24,8}};
       assertTrue("1.",  Arrays.deepEquals(res1, s.fun_matrixmultiply(arr1, arr2)));
-      // assertTrue("2.",  Arrays.deepEquals(res2, s.fun_matrixmultiply(arr3, arr4)));
+      assertTrue("2.",  Arrays.deepEquals(res2, s.fun_matrixmultiply(arr3, arr4)));
    }
 }
 
--- 01-matrixmultiply-Java/matrixmultiply.java
+++ 01-matrixmultiply-Java/matrixmultiply.java
@@ -10,16 +10,15 @@ class matrixmultiply {
 		if(a[0].length != b.length){
 			return new int[][]{};
 		}
-		// int[][] arr = new int[a.length][b[0].length];
-		// for(int i = 0; i < a.length; i++){
-		// 	for(int j = 0; j < b[0].length; j++){
-		// 		for(int k = 0; k < a[0].length; k++) {
-		// 			arr[i][j] = arr[i][j] + (a[i][k] * b[k][j]);
-		// 		}
-		// 	}
-		// }
-		// return arr;
-		return new int[0][0];
+		int[][] arr = new int[a.length][b[0].length];
+		for(int i = 0; i < a.length; i++){
+			for(int j = 0; j < b[0].length; j++){
+				for(int k = 0; k < a[0].length; k++) {
+					arr[i][j] = arr[i][j] + (a[i][k] * b[k][j]);
+				}
+			}
+		}
+		return arr;
 	}
 
 	public static void main(String[] args) {
