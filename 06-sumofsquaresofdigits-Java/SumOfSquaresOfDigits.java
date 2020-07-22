2229c3e9b3ab257bb3e26b79bd4ebe1a4e3d17a8
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-22 09:30:36 +0530

    All Test Cases Passed

--- 06-sumofsquaresofdigits-Java/SumOfSquaresOfDigits.java
+++ 06-sumofsquaresofdigits-Java/SumOfSquaresOfDigits.java
@@ -7,6 +7,16 @@
 public class SumOfSquaresOfDigits {
 	public long sumOfSquaresOfDigits(int n) {
 		// Your code goes here
-		return -1;
+		long sum = 0;
+		while(n > 0){
+			int d = n % 10;
+			sum = sum + (d * d);
+			n = n / 10;
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		
 	}
 }
