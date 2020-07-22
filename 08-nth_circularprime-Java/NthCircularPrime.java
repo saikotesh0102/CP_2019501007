71876c07d4e3ee2a90b3e6b02e99009c1be3feac
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-22 10:16:56 +0530

    Jul 22  10:16:55

--- 08-nth_circularprime-Java/NthCircularPrime.java
+++ 08-nth_circularprime-Java/NthCircularPrime.java
@@ -8,6 +8,59 @@
 public class NthCircularPrime {
 	public long nthCircularPrime(int n) {
 		// your code goes here
-		return -1;
+		if(n == 0){
+			return 2;
+		}	
+		int start = 3;
+		int count = 0;
+		while(true){
+			if(isCircularPrime(start) && isPrime(start)){
+				count = count + 1;
+				if(count == n){
+					return start;
+				}
+			}
+			start = start + 2;
+		}
+	}
+
+	public boolean isPrime(int n) { 
+        if (n <= 1){
+			return false;
+		}
+   
+        for (int i = 2; i < n; i++){
+            if (n % i == 0){
+				return false;
+			}
+		}
+        return true; 
+	}
+	
+	public boolean isCircularPrime(int n){
+		int count = 0;
+		int temp = n;
+
+		while(temp > 0){
+			count = count + 1;
+			temp = temp / 10;
+		}
+
+		int num = n;
+		while(isPrime(num)){
+			int rem = num % 10;
+			int div = num / 10;
+
+			num = (int)((Math.pow(10, count - 1) * rem) + div);
+
+			if(num == n){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		
 	}
 }
