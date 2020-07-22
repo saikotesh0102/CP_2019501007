7210f5e84a681e38397f234c591dd838b45d929c
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-22 09:49:44 +0530

    Jul 22  09:49:44

--- 07-nth_powerfulnumber-Java/NthPowerfulNumber.java
+++ 07-nth_powerfulnumber-Java/NthPowerfulNumber.java
@@ -7,6 +7,51 @@
 public class NthPowerfulNumber {
 	public long nthPowerfulNumber(int n) {
 		// Your code goes here
-		return -1;
-	}	
+		if(n == 0){
+			return 1;
+		}
+		int start = 2;
+		int count = 0;
+		while(true){
+			if(isPowerfulNumber(start)){
+				count = count + 1;
+				if(count == n){
+					return start;
+				}
+			}
+			start = start + 1;
+		}
+	}
+
+	public boolean isPowerfulNumber(int n){
+		while(n % 2 == 0){
+			int power = 0;
+			while(n % 2 == 0){
+				n = n / 2;
+				power = power + 1;
+			}
+
+			if(power == 1){
+				return false;
+			}
+		}
+
+		for(int i = 3; i < Math.sqrt(n); i = i + 2){
+			int power = 0;
+			while(n % i == 0){
+				n = n / i;
+				power = power + 1;
+			}
+
+			if(power == 1){
+				return false;
+			}
+		}
+		return n == 1;
+	}
+	
+	public static void main(String[] args) {
+		NthPowerfulNumber ob = new NthPowerfulNumber();
+		System.out.println(ob.nthPowerfulNumber(1));
+	}
 }
