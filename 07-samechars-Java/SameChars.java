7ed761c0822cad2b8d7c80f58c6f073379c8ad63
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-23 16:57:48 +0530

    Working

--- 07-samechars-Java/SameChars.java
+++ 07-samechars-Java/SameChars.java
@@ -8,6 +8,23 @@
 public class SameChars {
 	public boolean sameChars(String s1, String s2) {
 		// Your code goes here
+		if(s1.equals(s2)){
+			return true;
+		}
+		int count = 0;
+		for(int i = 0; i < s2.length(); i++){
+			if(s1.contains(s2.charAt(i))){
+				count = count + 1;
+			}
+		}
+
+		if(s2.length() == count){
+			return true;
+		}
 		return false;
+	}
+
+	public static void main(String[] args) {
+		
 	}
 }
