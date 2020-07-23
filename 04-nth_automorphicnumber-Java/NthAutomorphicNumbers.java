4bc6cf97e1c8f7ab346b16af2764c9b40d59237f
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-23 10:23:50 +0530

    Jul 23  10:23:50

--- 04-nth_automorphicnumber-Java/NthAutomorphicNumbers.java
+++ 04-nth_automorphicnumber-Java/NthAutomorphicNumbers.java
@@ -2,9 +2,39 @@
 // In mathematics, an automorphic number is a number whose square "ends" in the same digits as the 
 // number itself. For example, 5^2 = 25, 6^2 = 36, 76^2 = 5776, and 890625^2 = 793212890625, so 5, 6, 
 // 76 and 890625 are all automorphic numbers.
+
 public class NthAutomorphicNumbers {
 	public long nthAutomorphicNumbers(int n) {
 		// Your code goes here
-		return -1;
+		if(n == 1){
+			return 0;
+		}
+		int start = 1;
+		int count = 1;
+		while(true){
+			if(isAutomorphic(start)){
+				count = count + 1;
+				if(count == n){
+					return start;
+				}
+			}
+			start = start + 1;
+		}
+	}
+
+	public boolean isAutomorphic(int n){
+		int sq = n * n;
+		while(n > 0){
+			if(n % 10 != sq % 10){
+				return false;
+			}
+			n = n / 10;
+			sq = sq / 10;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		
 	}
 }
