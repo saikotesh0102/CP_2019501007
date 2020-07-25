f5e7846904439844ebcb7ff331d29a000448c7bf
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-25 13:47:01 +0530

    Question Updated

---
+++ 01-nthtenlyprime-Java/nthtenlyprime.java
@@ -0,0 +1,14 @@
+// # Write the function fun_nth_additive_prime(n) that takes a non-negative int n 
+// # and returns the nth Additive Prime, which is a prime number such that 
+// # the sum of its digits is also prime. For example, 113 is prime and 1+1+3==5 and 5 
+// # is also prime, so 113 is an Additive Prime. fun_nth_additive_prime(0) returns 2
+
+public class nthtenlyprime {
+	public int fun_nthtenlyprime(int n){
+		return 1;
+	}
+
+	public static void main(String[] args){
+		
+	}
+}
