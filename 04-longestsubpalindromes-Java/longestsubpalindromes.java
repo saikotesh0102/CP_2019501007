a8b3973fdd175b667a57c6f3319306c60d024c7e
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-25 14:38:31 +0530

    Updated

---
+++ 04-longestsubpalindromes-Java/longestsubpalindromes.java
@@ -0,0 +1,17 @@
+// # Write the function longestSubpalindrome(s), that takes a string s and returns 
+// the longest palindrome that occurs as consecutive characters (not just letters, but 
+// 	any characters) in s. So:
+// #    	longestSubpalindrome("ab-4-be!!!") 
+// # returns "b-4-b". If there is a tie, return the lexicographically larger value -- 
+// in java, a string s1 is lexicographically greater than a string s2 if (s1 > s2). So:
+// #    	longestSubpalindrome("abcbce") 
+// # returns "cbc", since ("cbc" > "bcb"). Note that unlike the previous functions, 
+// this function is case-sensitive (so "A" is not treated the same as "a" here). Also, 
+// from the explanation above, we see that longestSubpalindrome("aba") is "aba", 
+// and longestSubpalindrome("a") is "a".
+
+class longestsubpalindromes {
+	public String fun_longestsubpalindromes(String s){
+		return "";
+	}
+}
