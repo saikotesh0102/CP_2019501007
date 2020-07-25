bbd4c2263f3ff3630865dc9465e445ea4347ac68
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-25 19:53:14 +0530

    Jul 25  19:53:13

--- 04-longestsubpalindromes-Java/longestsubpalindromes.java
+++ 04-longestsubpalindromes-Java/longestsubpalindromes.java
@@ -11,35 +11,37 @@
 // and longestSubpalindrome("a") is "a".
 
 public class longestsubpalindromes {
-	public String fun_longestsubpalindromes(String s){
-		boolean[][] arr = new boolean[s.length()][s.length()];
-
-		for(int i = 0; i < s.length(); i++){
-			arr[i][i] = true;
+	public String fun_longestsubpalindromes(String str){
+		if(str.length() == 1){
+			return str;
 		}
+		String result = str.substring(0,1);
+		for(int i = 0; i < str.length(); i++){
+			String temp =  helper(str, i, i, str.length());
+			if(temp.length() > result.length()){
+				result = new String(temp);
+			}
 
-		int length = 1;
-		int start = 0;
-
-		for(int i = 2; i < s.length() + 1; i++){
-			for(int j = 0; j < s.length() - i + 1; j++){
-				int end = j + 1;
-				if (i == 2){
-					if(s.charAt(j) == s.charAt(end - 1)){
-						arr[j][end - 1] = true;
-						length = i;
-						start = j;
-					}
-				}else{
-					if(s.charAt(j) == s.charAt(end - 1) && arr[j + 1][end - 2]){
-						arr[j][end - 1] = true;
-						length = i;
-						start = j;
-					}
+			if(temp.length() == result.length()){
+				if(temp.compareTo(result) > 0){
+					result = new String(temp);
 				}
 			}
+
+			temp = helper(str, i, i + 1, str.length());
+			if(temp.length() > result.length()){
+				result = new String(temp);
+			}
 		}
-		return s.substring(start, start + length);
+		return result;
+	}
+
+	public String helper(String str, int begin, int end, int n){
+		while(begin >= 0 && end <= n -1 && str.charAt(begin) == str.charAt(end)){
+			begin = begin - 1;
+			end = end + 1;
+		}
+		return str.substring(begin + 1, end);
 	}
 
 	public static void main(String[] args) {
