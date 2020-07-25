6940db369c3aafd2cbc081bbd245b8f4e65a4ae6
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-25 15:52:58 +0530

    Working

--- 04-longestsubpalindromes-Java/longestsubpalindromes.java
+++ 04-longestsubpalindromes-Java/longestsubpalindromes.java
@@ -10,8 +10,39 @@
 // from the explanation above, we see that longestSubpalindrome("aba") is "aba", 
 // and longestSubpalindrome("a") is "a".
 
-class longestsubpalindromes {
+public class longestsubpalindromes {
 	public String fun_longestsubpalindromes(String s){
-		return "";
+		boolean[][] arr = new boolean[s.length()][s.length()];
+
+		for(int i = 0; i < s.length(); i++){
+			arr[i][i] = true;
+		}
+
+		int length = 1;
+		int start = 0;
+
+		for(int i = 2; i < s.length() + 1; i++){
+			for(int j = 0; j < s.length() - i + 1; j++){
+				int end = j + 1;
+				if (i == 2){
+					if(s.charAt(j) == s.charAt(end - 1)){
+						arr[j][end - 1] = true;
+						length = i;
+						start = j;
+					}
+				}else{
+					if(s.charAt(j) == s.charAt(end - 1) && arr[j + 1][end - 2]){
+						arr[j][end - 1] = true;
+						length = i;
+						start = j;
+					}
+				}
+			}
+		}
+		return s.substring(start, start + length);
+	}
+
+	public static void main(String[] args) {
+		
 	}
 }
