a9c611d637a901d51e0a0e278b2193666f099045
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-25 12:22:37 +0530

    All Test Cases Passed

--- 06-leastfrequentletters-Python/leastfrequentletters.py
+++ 06-leastfrequentletters-Python/leastfrequentletters.py
@@ -7,6 +7,14 @@
 # yet covered lists, sets, maps, or efficiency, you are not expected to write the most efficient solution. Finally, 
 # if s does not contain any alphabetic characters, the result should be the empty string ("")
 
+from collections import Counter
+
 def leastfrequentletters(s):
 	# Your code goes here
-	pass
+	string = ""
+	for each in s.lower():
+		if each.isalpha():
+			string = string + each
+	d = Counter(sorted(list(string)))
+	result = [k for k, v in d.items() if v == min(d.values())]
+	return "".join(result)
