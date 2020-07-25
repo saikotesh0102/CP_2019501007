765807c847def4e22deb745aff36d73f12a7e4c0
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-25 15:59:47 +0530

    Jul 25  15:59:47

--- 01-nthtenlyprime-Java/nthtenlyprime.java
+++ 01-nthtenlyprime-Java/nthtenlyprime.java
@@ -5,8 +5,50 @@
 
 public class nthtenlyprime {
 	public int fun_nthtenlyprime(int n){
-		return 1;
+		if(n == 0){
+			return 19;
+		}
+		int start = 20;
+		int count = 0;
+		while(true){
+			if(isTenlyPrime(start)){
+				count = count + 1;
+				if(count == n){
+					return start;
+				}
+			}
+			start = start + 1;
+		}
 	}
+
+	public boolean isTenlyPrime(int n){
+		int sum = 0;
+		int digit = n;
+		while(n > 0){
+			int rem = n % 10;
+			sum = sum + rem;
+			n = n / 10;
+		}
+
+		if(sum == 10 && isPrime(digit)){
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isPrime(int n) { 
+
+        if (n <= 1){
+			return false;
+		}
+
+        for (int i = 2; i < n; i++){ 
+            if (n % i == 0){
+				return false; 
+			}
+		}
+        return true; 
+    }
 
 	public static void main(String[] args){
 		
