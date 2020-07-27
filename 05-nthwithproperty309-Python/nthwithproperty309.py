bcbc814a23bbf1f8bbcc99f3d5c0b9d0df1d8c32
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-07-27 08:57:14 +0530

    All Test Cases Passed

--- 05-nthwithproperty309-Python/nthwithproperty309.py
+++ 05-nthwithproperty309-Python/nthwithproperty309.py
@@ -6,4 +6,14 @@
 
 def nthwithproperty309(n):
 	# Your code goes here
-	pass
+	if n == 0:
+		return 309
+	count = 0
+	start = 310
+	while True:
+		res = str(pow(start, 5))
+		if "0" in res and "1" in res and "2" in res and "3" in res and "4" in res and "5" in res and "6" in res and "7" in res and "8" in res and "9" in res:
+			count = count + 1
+			if count == n:
+				return start
+		start = start + 1
